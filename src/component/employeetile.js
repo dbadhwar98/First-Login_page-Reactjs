12c5b82784bea3d7a6644f51eb0cd6fbbad1bc14
Deeksha Badhwar <deekshabadhwar98@gmail.com>
2021-05-27 17:49:50 +0530

    assign1

--- src/component/employeetile.js
+++ src/component/employeetile.js
@@ -5,13 +5,13 @@ export default function employeetile(props) {
     <div>
 
       <h5>
-        {props.details.id}
+        Id: {props.details.id}
       </h5>
       <h5>
-        {props.details.name}
+         Name:{props.details.name}
         </h5>
         <h5>
-          {props.details.age}
+         Age: {props.details.age}
         </h5>
         ____________________________________________
     </div>
